965e418ec4f5f66982af514f97b5fa42792439a9
Anya <Anna.Lytvynova.PP.2023@lpnu.ua>
2026-04-05 21:48:21 +0300

    Update test

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function Buttons() {
 
   return (
     <div className="container">
-      <h1 className="title">Тестовий текст</h1>
+      <h1 className="title">Test</h1>
 
       <button className="button" onClick={() => setMessage("Натиснута перша кнопка")}>
         Кнопка 1
